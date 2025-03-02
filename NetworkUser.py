575c3f24b3c8923ac73f11b13f943d2cac9ca956
Juan <simpplay32@gmail.com>
2025-03-01 19:38:06 -0500

    Agregadas las conexiones a través de ssl

--- NetworkUser.py
+++ NetworkUser.py
@@ -1,4 +1,6 @@
 import socket as Socket
+import ssl
+
 import uuid as UUID
 import threading
 
@@ -17,6 +19,10 @@ DEFAULT_PROTOCOLS: list[type[NetworkProtocol]] = [
     ProductShareProtocol,
     FinalShareProtocol
 ]
+
+CERT_FILE = "ssl/cert.pem"
+KEY_FILE = "ssl/key.pem"
+HOSTNAME = "PC-Crypto"
 
 class NetworkUser:
     def __init__(self, host: Socket, uuid_str: str = ""):
@@ -40,6 +46,12 @@ class MainUser:
 
         self.party: dict[str, NetworkUser] = {}
 
+        self.server_context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+        self.server_context.load_cert_chain(certfile=CERT_FILE, keyfile=KEY_FILE)
+
+        self.client_context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
+        self.client_context.load_verify_locations(CERT_FILE)
+
         self._input_shares: dict[str, Protocol.SharedVariable] = {}
         self._multiplication_gates: dict[int, Protocol.MultiplicationGate] = {}
         self.final_shares: list[Protocol.SharedVariable] = []
@@ -47,18 +59,20 @@ class MainUser:
         self.server_thread: threading.Thread = threading.Thread(target=self.start_server, daemon=True)
         self.server_thread.start()
 
+        self.addConnection(self.ip, self.port, self.uuid)
+
     def start_server(self):
         self.server = Socket(AF_INET, SOCK_STREAM)
         self.server.bind((self.ip, self.port))
         self.server.listen(5)
         self.log(f"Servidor iniciado en {self.ip}:{self.port}")
-        self.addConnection(self.ip, self.port, self.uuid)
         while True:
             connection, _ = self.server.accept()
             threading.Thread(target=self.handle_client, args=(connection,), daemon=True).start()
 
     def handle_client(self, connection: Socket):
         buffer = ""
+        connection = self.server_context.wrap_socket(connection, server_side=True)
         while True:
             try:
                 data = connection.recv(1024).decode("utf-8")
@@ -69,7 +83,7 @@ class MainUser:
                     message, buffer = buffer.split(DELIMITADOR, 1)
                     self.receive(message)
             except Exception as e:
-                self.log(f"[-] Error al recibir mensaje: {e} \nBuffer: {buffer}") # TODO: Handle disconnection
+                # TODO: Hacer PING PONG con toda la party para saber si se desconectó alguien
                 break
     
     @property
@@ -100,25 +114,26 @@ class MainUser:
             protocol.send_message(user.host, message)
 
     def connect(self, ip: str, port: int):
-        connection = None
+        secure_connection = None
         try:
             connection = Socket(AF_INET, SOCK_STREAM)
-            connection.connect((ip, port))
+
+            secure_connection = self.client_context.wrap_socket(connection, server_hostname=HOSTNAME)
+            secure_connection.connect((ip, port))
+            
             protocol = RequestConnectionProtocol(self)
-            protocol.send_message(connection)
+            protocol.send_message(secure_connection)
             self.log(f"Enviando solicitud de conexión a {ip}:{port}")
         except Exception as e:
             self.log(f"No se pudo conectar a {ip}:{port}:{str(e)}")
-        finally: 
-            if connection:
-                connection.close()
 
     def addConnection(self, ip: str, port: int, uuid: str) -> NetworkUser | None:
         if uuid in self.party:
             return None
         connection = Socket(AF_INET, SOCK_STREAM)
-        connection.connect((ip, port))
-        user = NetworkUser(connection, uuid)
+        secure_connection = self.client_context.wrap_socket(connection, server_hostname=HOSTNAME)
+        secure_connection.connect((ip, port))
+        user = NetworkUser(secure_connection, uuid)
         self.party[uuid] = user
         self.party = dict(sorted(self.party.items()))
         self.log(f"Conexión establecida con {uuid} | {ip}:{port}")
