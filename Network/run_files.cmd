b3dc6e42c93577f111e3855da2b4a4c385da21c7
Juan <simpplay32@gmail.com>
2025-03-07 03:49:32 -0500

    Finished Network

--- Network/run_files.cmd
+++ Network/run_files.cmd
@@ -4,5 +4,5 @@ set n=6
 
 for /L %%i in (1,1,%n%) do (
     set /A port=5000 + %%i
-    start "" python main.py --ip 192.168.1.5 --port !port! --uuid %%i --file connections.json
+    start "" python main.py --port !port! --uuid %%i --file connections.json
 )
--- run_files.bash
+++
@@ -1,4 +0,0 @@
-n=6
-for i in $(seq 1 $n); do
-  python3 main.py --port $((5000 + i)) --uuid $i --file connections.json &
-done
